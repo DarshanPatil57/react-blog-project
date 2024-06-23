c6f4c07e3ead58e05986ee11e73a21fe7dd3d053
Darshan Patil <5303darshan@gmail.com>
2024-06-23 20:59:36 +0530

    feat: add store and components for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './Appwrite/auth'
+import {login,logout} from './Store/authSlice'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  const [loading,setLoading] = useState(true)
+  const dipatch = useDispatch()
 
-  return (
-    <>
-    <h1>A blog app with appwrite</h1>
-    </>
-  )
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+
+      if(userData){
+        dipatch(login({userData}))
+      }
+      else{
+        dipatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className=' max-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          <Outlet/> 
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
